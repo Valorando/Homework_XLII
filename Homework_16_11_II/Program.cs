667d991f6e25024ae0de867aa28f3d51bd7725e9
Valorando <vadimsokil.dnipro@gmail.com>
2023-11-15 22:37:47 +0200

    Добавьте файлы проекта.

---
+++ Homework_16_11_II/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_16_11_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
